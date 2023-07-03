470538b446545d6ee64e01a5372d9b7ae8b02e33
tayleredelen <taylerledelen@gmail.com>
2023-07-03 11:10:55 -0500

    Completed Part 2: Add Another Component

--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,14 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+import classes from './ChoresList.module.css';
+
+export default function ChoresList() {
+      const chores = ["Fix React Textbook", "It doesn't ever say to add chores", "Or how to implement the CSS changes we make"];
+  return (
+    <div>
+      <p className={classes.choresText}>The textbook never said to add chores</p>
+      <h1 className={classes.choresHeading}>Chores List</h1>
+            <li className={classes.choresText}>{chores[0]}</li>
+            <li className={classes.choresText}>{chores[1]}</li>
+            <li className={classes.choresText}>{chores[2]}</li>
+    </div>
+  );
 }
