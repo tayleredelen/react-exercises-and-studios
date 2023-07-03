c9b1f3bca4663944f12ca23d132aeca6d5508b17
tayleredelen <taylerledelen@gmail.com>
2023-07-03 11:44:44 -0500

    Edited tags in HobbyLinks.js to be empty (fragments)

--- part1/exercises/src/components/HobbyLinks.js
+++ part1/exercises/src/components/HobbyLinks.js
@@ -3,10 +3,10 @@ export default function HobbyLinks() {
 let hobbyLinks = ["https://www.riseart.com/guide/2371/a-guide-to-collage", "https://unsplash.com/s/photos/collage-art"];
 
 return(
-<div>
+<>
 <a href = {hobbyLinks[0]}>A Guide To Collage</a>
 <br/>
 <a href = {hobbyLinks[1]}>Collage Art Photos</a>
-</div>
+<>
 );
 }
