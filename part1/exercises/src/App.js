c215bd0cbe7e6a17f991a461977db2bb8ee2a05d
tayleredelen <taylerledelen@gmail.com>
2023-07-03 10:54:10 -0500

    Completed Part 2: Add Another Component

--- part1/exercises/src/App.js
+++ part1/exercises/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import MovieList from './components/MovieList.js';
 import ChoresList from './components/ChoresList.js';
 import BookList from './components/BookList.js';
+import HobbyLinks from './components/HobbyLinks.js';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <MovieList />
       <ChoresList />
       <BookList />
+      <HobbyLinks />
     </div>
   );
 }
---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,10 @@
+function HobbyLinks() {
+
+const hobbyLinks = ["https://www.riseart.com/guide/2371/a-guide-to-collage", "https://unsplash.com/s/photos/collage-art"]
+
+return(
+<a href={hobbyLinks[0]}>A Guide To Collage</a>
+<a href={hobbyLinks[1]}>Collage Art Photos</a>
+
+);
+}
