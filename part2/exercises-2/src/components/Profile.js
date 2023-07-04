352d258fed2d4c984a3b4f6188575425a4b3750c
tayleredelen <taylerledelen@gmail.com>
2023-07-04 12:08:29 -0500

    Completed A&B of Part 3: Profiles Component

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -8,11 +8,34 @@ import React from 'react';
 import oceans from './oceans.json';
 
 
-function Profile()
-{
+function Profile() {
+const listItem = oceans.map(ocean => (
+//We will use map to help us iterate through the image values in the JSON file.
+//Declare a variable called listItem and instantiate it equal to oceans.map().
+//Set the callback parameter of listItem to ocean =>
+//The map function should return ocean which will include all elements required to create a profile object.
+   <div key={ocean.id}>
+//   Each profile will be contained in its own <div>.
+//To keep each <div> unique, we will need to add a key which is set to the id property of each JSON object.
+//We are working with JSON objects, so you will need curly braces.
+      <img src={ocean.image} alt={ocean.name} className="img"/>
+//Using an image tag, set the src attribute equal to the image’s url. This is stored in the JSON file.
+//The alt attribute should be set to the name property.
+//The starter code contains some CSS styling for the image called img.
+//Apply the styling to the image by using the className attribute.
+  <h2>{ocean.name}</h2>
+  <ul>
+  <li>{ocean.fact1}</li>
+  <li>{ocean.fact2}</li>
+  <li>{ocean.fact3}</li>
+  </ul>
+   </div>
+));
    return(
       <div>
+         <ul>{listItem}</ul>
          <Button />
+//The Profile function needs to return an list of listItems and the <Button />.
       </div>
    );
 }
