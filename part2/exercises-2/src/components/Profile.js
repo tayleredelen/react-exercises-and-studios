3788fa42d9154dab9af764c409f0b39032ba3b5f
tayleredelen <taylerledelen@gmail.com>
2023-07-04 12:17:06 -0500

    Completed Exercise for Part 2

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -10,18 +10,23 @@ import oceans from './oceans.json';
 
 function Profile() {
 const listItem = oceans.map(ocean => (
-   <div key={ocean.id}>
+<div key={ocean.id}  className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
       <img src={ocean.image} alt={ocean.name} className="img"/>
+      <h1>{ocean.name}</h1>
+       <h3>Fun Facts:</h3>
+         <ul>
+            <li>{ocean.fact1}</li>
+            <li>{ocean.fact2}</li>
+            <li>{ocean.fact3}</li>
+         </ul>
+            <Button />
    </div>
 ));
 
    return(
-      <>
          <ul>
             {listItem}
          </ul>
-         <Button />
-      </>
    );
 }
 
